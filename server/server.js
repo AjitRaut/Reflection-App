9e8bd63f16e97770b6076b9596c3b3b7fae77140
Ajit raut <ajitraut9561@gmail.com>
2024-12-06 21:12:11 +0530

    Server Added

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const feedbackRoutes = require("./routes/feedbackRoutes");
+
+require("dotenv").config();
+const app = express();
+
+connectDB();
+app.use(express.json());
+app.use(require("cors")());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/feedback", feedbackRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
